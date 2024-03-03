a82085127273d1a15f66e12fc46bdedeeecb7031
oseongryu <oseongryu@gmail.com>
2024-03-03 17:45:56 +0900

    feat: crawling view setting

--- crawling/views.py
+++ crawling/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# from django.http import Http404
+# from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.db.models import F
+
+class IndexView(generic.ListView):
+    template_name = "crawling/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        print('test')
+        return
