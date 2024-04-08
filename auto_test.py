9adc3f044fc83dabff76e0e5d4e1832556e19cbe
oseongryu <oseongryu@gmail.com>
2024-04-08 13:06:30 +0900

    fix: auto

--- auto_test.py
+++ auto_test.py
@@ -5,6 +5,10 @@ from pynput import keyboard
 # print(pyautogui.size())
 temp = True
 
+# pyautogui.FailSafeException: PyAutoGUI fail-safe triggered from mouse moving to a corner of the screen. To disable this fail-safe, set pyautogui.FAILSAFE to False. DISABLING FAIL-SAFE IS NOT RECOMMENDED
+pyautogui.FAILSAFE = False
+
+
 # while True:
 #     if keyboard.read_key() == "p":
 #         print("You pressed p")
@@ -44,16 +48,16 @@ while(temp):
 
     # mac = 손쉬운 사용 vscode 사용 권한 설정
     # pyautogui.moveTo(100, 200) # x 100, y 200 위치로 바로 이동
-    pyautogui.moveTo(700, 200, 2) # x 100, y 200 위치로 2초동안 이동
+    pyautogui.moveTo(1910, 500, 2) # x 100, y 200 위치로 2초동안 이동
 
-    pyautogui.click()
+    # pyautogui.click()
     pyautogui.click(button='right')
     # pyautogui.doubleClick()
-    pyautogui.click(clicks=3, interval=1) # 3번 클릭할건데 1초마다
+    # pyautogui.click(clicks=3, interval=1) # 3번 클릭할건데 1초마다
 
     # 816,81 -> 539,80
-    pyautogui.moveTo(816,81, 2)
+    pyautogui.moveTo(900,500, 2)
     # pyautogui.dragTo(539,80, 2)
 
-    pyautogui.moveTo(100, 200, 2) # x 100, y 200 위치로 2초동안 이동
+    pyautogui.moveTo(100, 500, 2) # x 100, y 200 위치로 2초동안 이동
     print('end')
